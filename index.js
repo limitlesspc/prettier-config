0b692b9c29c1112020d74eb13c70c34dedc27f9c
limitlesspc <48165199+limitlesspc@users.noreply.github.com>
2025-12-18 15:20:48 -0500

    feat: add experimentalOperatorPosition option in prettier v3.5

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /** @type {import('prettier').Options} */
 module.exports = {
   experimentalTernaries: true,
+  experimentalOperatorPosition: true,
   quoteProps: "consistent",
   arrowParens: "avoid",
 };
